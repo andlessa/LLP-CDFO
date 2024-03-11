89e8d60e1e0ad83652d4d7cd82b95def7b939bfc
Andre Lessa <lessa.a.p@gmail.com>
2024-03-11 08:30:10 -0300

    Implementing hscp for rhadrons

--- helper.py
+++ helper.py
@@ -2,9 +2,9 @@
 
 from typing import Any
 import numpy as np
-import os,glob
+import os
+import glob
 import pyslha
-import re
 
 class LLP(object):
     """
@@ -20,6 +20,16 @@ class LLP(object):
         self._selectedDecays = []
         self.nTracks = None
         self.mDV = None
+        self.Xd = np.inf
+        self.Yd = np.inf
+        self.Zd = np.inf
+        self.r_decay = np.inf
+        self.z_decay = np.inf
+        trimom = np.sqrt(self.Px**2 +self.Py**2 + self.Pz**2)
+        self.beta = trimom/self.E
+        self.gbeta = trimom/self.Mass
+
+
 
         for d in self.finalDaughters:
             if d.Charge == 0:
@@ -49,38 +59,36 @@ class LLP(object):
         self.nTracks = len(self._selectedDecays)
 
         # Consistency checks:
-        pTot = np.array([self.E,self.Px,self.Py,self.Pz])
-        pNorm = np.linalg.norm(pTot)
-        for d in self.directDaughters:                
-            pTot -= np.array([d.E,d.Px,d.Py,d.Pz])
-        if self.directDaughters and np.linalg.norm(pTot)/pNorm > maxMomViolation/1e3: # Be more strict about direct daughters
-            # raise ValueError("Error getting direct daughters, momentum conservation violated by %1.1e!" %(np.linalg.norm(pTot)/pNorm))
-            print("Error getting direct daughters, momentum conservation violated by %1.1e!" %(np.linalg.norm(pTot)/pNorm))
-      
-        pTot = np.array([self.E,self.Px,self.Py,self.Pz])
-        pNorm = np.linalg.norm(pTot)
-        for d in self.finalDaughters:                
-            pTot -= np.array([d.E,d.Px,d.Py,d.Pz])
-        if self.finalDaughters and np.linalg.norm(pTot)/pNorm > maxMomViolation:
-            # raise ValueError("Error getting final daughters, momentum conservation violated by %1.1e! (%s)" %(np.linalg.norm(pTot)/pNorm,str(pTot)))
-            print("Error getting final daughters, momentum conservation violated by %1.1e! (%s)" %(np.linalg.norm(pTot)/pNorm,str(pTot)))
-        
-        
-        rList = [np.sqrt(d.X**2 + d.Y**2 + d.Z**2) for d in self.directDaughters]
+        if self.directDaughters:
+            pTot = np.array([self.E,self.Px,self.Py,self.Pz])
+            pNorm = np.linalg.norm(pTot)
+            for d in self.directDaughters:                
+                pTot -= np.array([d.E,d.Px,d.Py,d.Pz])
+            if self.directDaughters and np.linalg.norm(pTot)/pNorm > maxMomViolation/1e3: # Be more strict about direct daughters
+                # raise ValueError("Error getting direct daughters, momentum conservation violated by %1.1e!" %(np.linalg.norm(pTot)/pNorm))
+                print("Error getting direct daughters, momentum conservation violated by %1.1e!" %(np.linalg.norm(pTot)/pNorm))
 
-        if max(rList) != min(rList) and abs(max(rList)-min(rList))/(max(rList)+min(rList)) > 0.01:
-            raise ValueError("Direct daughters do not have the same production vertex!")
-        
-        daughter = self.directDaughters[0]
-        self.Xd = daughter.X
-        self.Yd = daughter.Y
-        self.Zd = daughter.Z
-        self.r_decay = np.sqrt(self.Xd**2+self.Yd**2)
-        self.z_decay = self.Zd
+            rList = [np.sqrt(d.X**2 + d.Y**2 + d.Z**2) for d in self.directDaughters]
 
-        trimom = np.sqrt(self.Px**2 +self.Py**2 + self.Pz**2)
-        self.beta = trimom/self.E
-        self.gbeta = trimom/self.Mass
+            if max(rList) != min(rList) and abs(max(rList)-min(rList))/(max(rList)+min(rList)) > 0.01:
+                raise ValueError("Direct daughters do not have the same production vertex!")
+            
+            daughter = self.directDaughters[0]
+            self.Xd = daughter.X
+            self.Yd = daughter.Y
+            self.Zd = daughter.Z
+            self.r_decay = np.sqrt(self.Xd**2+self.Yd**2)
+            self.z_decay = self.Zd
+
+        if self.finalDaughters:      
+            pTot = np.array([self.E,self.Px,self.Py,self.Pz])
+            pNorm = np.linalg.norm(pTot)
+            for d in self.finalDaughters:                
+                pTot -= np.array([d.E,d.Px,d.Py,d.Pz])
+            if self.finalDaughters and np.linalg.norm(pTot)/pNorm > maxMomViolation:
+                # raise ValueError("Error getting final daughters, momentum conservation violated by %1.1e! (%s)" %(np.linalg.norm(pTot)/pNorm,str(pTot)))
+                print("Error getting final daughters, momentum conservation violated by %1.1e! (%s)" %(np.linalg.norm(pTot)/pNorm,str(pTot)))
+            
 
 
     def __getattr__(self, attr: str) -> Any:
@@ -91,6 +99,12 @@ class LLP(object):
                 return self._candidate.__getattribute__(attr)
             except:
                 raise AttributeError("Could not get attribute %s" %attr)
+            
+    def __str__(self):
+        return "%s (%s)" %(self.PID,self.Status)
+    
+    def __repr__(self) -> str:
+        return str(self)
 
 
 def getLLPs(llpList,directDaughters,finalDaughters,maxMomViolation=5e-2,trackEff=1.0):
