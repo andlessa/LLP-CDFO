d8f1f05415f1e33a6194740a58cfb05a27479283
Andre Lessa <lessa.a.p@gmail.com>
2024-05-21 15:23:15 -0300

    Added HighLumi projections

---
+++ ATLAS-SUSY-2018-42/atlas_susy_2018_42_UpperLimits_HighLuminosity.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import time,os,sys
+import pandas as pd
+
+
+def computeULs(inputFile,outputFile):
+
+
+    # ### Load ATLAS data
+    atlasFile = './ATLAS_data/modelIndependentLimits_HL.pcl'
+    atlasDF = pd.read_pickle(atlasFile)
+
+    # ### Load Recast Data
+    recastDF = pd.read_pickle(inputFile)
+
+    # Merge data frames based on Target Mass and SR
+    allDF = pd.merge(recastDF,atlasDF,on=['Target Mass [GeV]','SR', 'Regime'],how="inner",
+                     suffixes=(None,"del_me"))
+    lumi = 3000.0
+    allDF.drop(columns=[c for c in allDF.columns if "del_me" in c],inplace=True)
+    allDF['robs'] = allDF['$N_s$']*(lumi/allDF['Luminosity (1/fb)'])/allDF['S95_obs_HL']
+    allDF['rexp'] = allDF['$N_s$']*(lumi/allDF['Luminosity (1/fb)'])/allDF['S95_exp_HL']
+    allDF['robsErr'] = allDF['$\\sigma_{Ns}$']*(lumi/allDF['Luminosity (1/fb)'])/allDF['S95_obs_HL']
+    allDF['rexpErr'] = allDF['$\\sigma_{Ns}$']*(lumi/allDF['Luminosity (1/fb)'])/allDF['S95_exp_HL']
+
+    # Filter data, so only the highest robs is kept
+    mLLP = 'mLLP'
+    mn1= 'mLSP'
+    tau = 'tau_ns'
+    mCols = [mLLP,mn1,tau,'robs']
+    allDF.sort_values(mCols,inplace=True,ascending=True)
+    recastDF2 = allDF.drop_duplicates(subset=[mLLP,mn1,tau],keep="last")
+
+    # Store all data to the output file
+    recastDF2.to_pickle(outputFile)
+
+
+
+if __name__ == "__main__":
+    
+    import argparse    
+    ap = argparse.ArgumentParser(description=
+            "Compute the upper limit on mu for ATLAS-SUSY-2018-42 for the recast data stored in the input file. \
+            The points without any signal will be removed from the output.")
+    ap.add_argument('-f', '--inputFile', required=True,
+            help='path to the pickle file containing the Pandas DataFrame with the recasting results for the models')
+    ap.add_argument('-o', '--outputFile', required=False,
+            help='path to output file. If not defined the upper limits will be stored in the input file.',
+            default = None)
+
+    t0 = time.time()
+
+    # # Set output file
+    args = ap.parse_args()
+    inputFile = args.inputFile
+    if not os.path.isfile(inputFile):
+        print("File %s not found" %inputFile)
+        sys.exit()
+    outputFile = args.outputFile
+    if outputFile is None:
+        outputFile = inputFile.replace('.pcl','_HL.pcl')
+
+    computeULs(inputFile,outputFile)
+
+    print("\n\nDone in %3.2f min" %((time.time()-t0)/60.))
+
+
+
+
+
--- installer.sh
+++ installer.sh
@@ -29,13 +29,13 @@ fi
 cd $homeDIR
 
 
-madgraph="MG5_aMC_v3.5.3.tar.gz"
+madgraph="MG5_aMC_v3.5.4.tar.gz"
 URL=https://launchpad.net/mg5amcnlo/3.0/3.5.x/+download/$madgraph
 echo -n "Install MadGraph (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
 	mkdir MG5;
-	echo "[installer] getting MadGraph5"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $madgraph -C MG5 --strip-components 1;
+	echo "[installer] getting MadGraph5 ($URL)"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $madgraph -C MG5 --strip-components 1;
 	cd $homeDIR
 	cd ./MG5/bin;
 	echo "[installer] installing HepMC, LHAPDF6 and Pythia8 under MadGraph5"
